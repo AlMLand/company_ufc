ba4db63150c2cbd0f26dae9273eb548438c951dc
AlMLand <morlandalex@googlemail.com>
2021-01-11 20:27:46 +0100

    Modified one argument.

--- ufc/src/main/java/com/m_landalex/ufc/mapper/FightMapper.java
+++ ufc/src/main/java/com/m_landalex/ufc/mapper/FightMapper.java
@@ -4,12 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import com.m_landalex.ufc.data.Fight;
-import com.m_landalex.ufc.domain.FighterEntity;
+import com.m_landalex.ufc.domain.FightEntity;
 
 @Component
-public class FightMapper extends AbstractMapper<Fight, FighterEntity> {
+public class FightMapper extends AbstractMapper<Fight, FightEntity> {
 
-	public FightMapper(ModelMapper modelMapper, Class<Fight> dtoClass, Class<FighterEntity> entityClass) {
+	public FightMapper(ModelMapper modelMapper, Class<Fight> dtoClass, Class<FightEntity> entityClass) {
 		super(modelMapper, dtoClass, entityClass);
 	}
 
